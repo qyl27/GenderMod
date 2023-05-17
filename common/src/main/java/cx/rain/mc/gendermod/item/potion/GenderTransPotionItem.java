529e6186476da073da0dbd0adbc3c064ffedbf84
秋雨落 <i@rain.cx>
2023-05-17 09:08:21 +0800

    :sparkles: Inventory tick.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- common/src/main/java/cx/rain/mc/gendermod/item/potion/GenderTransPotionItem.java
+++ common/src/main/java/cx/rain/mc/gendermod/item/potion/GenderTransPotionItem.java
@@ -11,12 +11,14 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.PotionItem;
+import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gameevent.GameEvent;
@@ -79,5 +81,14 @@ public class GenderTransPotionItem extends PotionItem {
     @Override
     public ItemStack getDefaultInstance() {
         return new ItemStack(this);
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, Level level, Entity entity, int slotId, boolean isSelected) {
+        super.inventoryTick(stack, level, entity, slotId, isSelected);
+
+        if (PotionUtils.getPotion(stack) != GModPotion.GENDER_TRANSFORM.get()) {
+            PotionUtils.setPotion(stack, GModPotion.GENDER_TRANSFORM.get());
+        }
     }
 }
